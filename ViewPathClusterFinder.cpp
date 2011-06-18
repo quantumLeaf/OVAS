95398dd94e4192af0d9732dde2b2f5c65ea73701
Liam Kurmos <quantum.leaf@gmail.com>
2011-06-18 10:19:21 +0100

    110608

---
+++ ViewPathClusterFinder.cpp
@@ -0,0 +1,22 @@
+/* 
+ * File:   ViewPathClusterFinder.cpp
+ * Author: zoizoi
+ * 
+ * Created on 18 June 2011, 10:15
+ */
+
+#include "ViewPathClusterFinder.h"
+
+ViewPathClusterFinder::ViewPathClusterFinder() {
+}
+
+ViewPathClusterFinder::ViewPathClusterFinder(const ViewPathClusterFinder& orig) {
+}
+
+ViewPathClusterFinder::~ViewPathClusterFinder() {
+}
+
+ViewPathClusterFinder::testCluserDetection(){
+    
+}
+
